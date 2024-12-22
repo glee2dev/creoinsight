149e2a35c481a55fd992244e0e35206f380999f2
glee2dev <glee2dev@gmail.com>
2024-12-22 17:45:53 +0900

    mod

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/creoinsight' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/creoinsight/' : '',
+  }
 }
 
 module.exports = nextConfig
